22d62f5d2a7c2215066a46b58e52f44794e1be65
msfernandes <matheus.souza.fernandes@gmail.com>
2014-09-22 16:57:53 -0300

    interface de login atualizada

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,11 @@
-# from django.template import RequestContext
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 # Create your views here.
 
 
 def sign_in(request):
-    return render_to_response('sign_in.html')
+    return render_to_response(
+        'sign_in.html',
+        context_instance=RequestContext(request)
+    )
