717bf1bdc2aa40f57ae9120b0a97eb3cdb643642
msfernandes <matheus.souza.fernandes@gmail.com>
2014-09-22 17:30:30 -0300

    Criacao do app Core e roteando url / para a home

--- anato/urls.py
+++ anato/urls.py
@@ -5,9 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'anato.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
     url(r'^login/', include('authentication.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'core.views.home_search'),
 )
