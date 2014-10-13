680c616c0d13ac489a01b2ac5feb050e64834a62
msfernandes <matheus.souza.fernandes@gmail.com>
2014-10-13 17:00:33 -0300

    Updating ldap authentication

--- authentication/auth_exceptions.py
+++ authentication/auth_exceptions.py
@@ -8,7 +8,16 @@ class LDAPConnectionError(Exception):
     def __str__(self):
         return self.message
 
+
 class LDAPCredentialError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+class LDAPUserDoesNotExist(Exception):
     def __init__(self, message):
         self.message = message
 
--- authentication/ldap_auth.py
+++ authentication/ldap_auth.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import ldap
-from auth_exceptions import LDAPConnectionError, LDAPCredentialError
-from configs.ldap import LDAP_DOMAIN, LDAP_SERVER
+from auth_exceptions import LDAPConnectionError, LDAPCredentialError, \
+    LDAPUserDoesNotExist
+from configs.ldap import LDAP_DOMAIN, LDAP_SERVER, DN, DN_SECRET, \
+    LDAP_BASE
+
 
 def initialize_ldap_connection():
     server = LDAP_SERVER
@@ -10,17 +13,35 @@ def initialize_ldap_connection():
     connection = ldap.initialize(server)
     connection.protocol_version = ldap.VERSION3
     connection.set_option(ldap.OPT_REFERRALS, 0)
+    connection.simple_bind_s(DN, DN_SECRET)
 
     return connection
 
-def ldap_authentication(username, password):
-    connection = initialize_ldap_connection()
-    username = username + LDAP_DOMAIN
 
-    try:
-        connection.simple_bind_s(username, password)
+def search_user(connection, username):
+    scope = ldap.SCOPE_SUBTREE
+    filter = "(&(sAMAccountName=" + username + "))"
+    timeout = 60
 
+    search = connection.search(LDAP_BASE, scope, filter)
+    user_type, user = connection.result(search, timeout)
+
+    if user[0][0] is not None:
         return True
+    else:
+        return False
+
+
+def ldap_autentication(username, password):
+    try:
+        connection = initialize_ldap_connection()
+
+        if search_user(connection, username):
+            username = username + LDAP_DOMAIN
+            connection.simple_bind_s(username, password)
+            return True
+        else:
+            raise LDAPUserDoesNotExist("User does not exist")
     except ldap.INVALID_CREDENTIALS:
         raise LDAPCredentialError("Invalid Credentials")
     except ldap.SERVER_DOWN:
