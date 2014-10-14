b57abf39caa173fc432b85c59b50b2dd67b86d2e
msfernandes <matheus.souza.fernandes@gmail.com>
2014-10-14 11:11:57 -0300

    App Exam'

---
+++ exam/urls.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns(
+    'exam.views',
+    url(r'^novo/$', 'new_exam'),
+    url(r'^registrar/$', 'register_exam'),
+)
