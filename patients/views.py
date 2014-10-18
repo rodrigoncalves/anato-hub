737f7cce7c7bc8f88ad38a773fe95313e366662f
Filipe Borges <filipe_bkc@yahoo.com.br>
2014-10-18 08:32:28 -0300

    Updating Search Patient

--- patients/views.py
+++ patients/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def search_results(request):
-    patient = request.POST['patient']
-    report = request.POST['report']
-    date = request.POST['date']
-    mother = request.POST['mother_name']
+    #patient = request.POST['patient']
+    #report = request.POST['report']
+    #date = request.POST['date']
+    #mother = request.POST['mother_name']
 
-    dictionary_params = {}
+    #dictionary_params = {}
 
     #criar metodo para fazer busca no banco de dados
 
     return render_to_response(
         'search_results.html',
-        {'patient': patient, 'report': report, 'date': date, 'mother': mother},
+        #{'patient': patient, 'report': report, 'date': date, 'mother': mother},
         context_instance=RequestContext(request)
     )
 
