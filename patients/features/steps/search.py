924f16167ba6686a24de0012ec54f9d381232377
Filipe Borges <filipe_bkc@yahoo.com.br>
2014-10-18 11:51:18 -0300

    Partial implementation BDD - Seach Patient

---
+++ patients/features/steps/search.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+from behave import given, when, then
+from selenium import webdriver
+from should_dsl import should, should_not
+
+
+@given(u'que o Usuario esta na tela de busca de pacientes')
+def is_authenticated(context):
+    context.driver = webdriver.Firefox()
+    #Alterar porta do localhost
+    context.driver.get('http://localhost:8000/consulta/')
+    context.driver.title | should | equal_to('Home | Anato')
+
+@given(u'aparece a tela de busca')
+def step_impl(context):
+    assert False
+
+@when(u'o usuario digita o nome do Paciente')
+def typing_pacient_name(context):
+    pacient_name_input = context.driver.find_element_by_id('patient')
+    pacient_name_input.send_keys('Queilane')
+
+@when(u'clica em buscar')
+def click_search_button(context):
+    search_button = context.driver.find_element_by_id('search-button')
+    search_button.click()
+
+@then(u'o sistema retorna os Pacientes com o nome digitado')
+def return_pacient_name(context):
+    pacient_name = context.driver.find_element_by_class_name('fi-male')
+    pacient_name.getText() | should | equal_to('QUEILANE BXXXISTA')
+
+@when(u'o usuario digita o prontuario do Paciente')
+def typing_patientreport_only(context):
+    assert False
+
+@then(u'o sistema retorna os Pacientes com o prontuario digitado')
+def step_impl(context):
+    assert False
+
+@when(u'o usuario digita o nome da mae do Paciente')
+def typing_mothername_only(context):
+    assert False
+
+@then(u'o sistema retorna os Pacientes com o nome da mae digitado')
+def step_impl(context):
+    assert False
+
+@when(u'o usuario digita a data de nascimento do Paciente')
+def typing_birthday_only(context):
+    assert False
+
+@then(u'o sistema retorna os Pacientes com a data de nascimento digitada')
+def step_impl(context):
+    assert False
+
+@when(u'o usuario digita o nome e prontuario do Paciente')
+def step_impl(context):
+    assert False
+
+@then(u'o sistema retorna os Pacientes com a nome e prontuario digitado')
+def step_impl(context):
+    assert False
+
+@when(u'o usuario digita o nome e nome da mae do Paciente')
+def step_impl(context):
+    assert False
+
+@then(u'o sistema retorna os Pacientes com a nome e nome da mae digitado')
+def step_impl(context):
+    assert False
+
+@when(u'o usuario digita o nome e data de nascimento do Paciente')
+def step_impl(context):
+    assert False
+
+@then(u'o sistema retorna os Pacientes com a nome e data de nascimento digitado')
+def step_impl(context):
+    assert False
+
+@when(u'o usuario digita o prontuario e nome da mae do Paciente')
+def step_impl(context):
+    assert False
+
+@then(u'o sistema retorna os Pacientes com a prontuario e nome da mae digitado')
+def step_impl(context):
+    assert False
+
+@when(u'o usuario digita o prontuario e data nascimento do Paciente')
+def step_impl(context):
+    assert False
+
+@then(u'o sistema retorna os Pacientes com a prontuario e data nascimento digitado')
+def step_impl(context):
+    assert False
+
+@when(u'o usuario digita o nome da mae e data nascimento do Paciente')
+def step_impl(context):
+    assert False
+
+@then(u'o sistema retorna os Pacientes com a nome da mae e data nascimento digitado')
+def step_impl(context):
+    assert False
