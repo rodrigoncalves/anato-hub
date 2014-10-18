6f1c0f4dd2a00470740d8c8b0e681036f5881dff
Filipe Borges <filipe_bkc@yahoo.com.br>
2014-10-18 10:46:49 -0300

    Implemented search patients

--- patients/views.py
+++ patients/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from patients.models import Paciente
 
 def search_results(request):
-    #patient = request.POST['patient']
-    #report = request.POST['report']
-    #date = request.POST['date']
-    #mother = request.POST['mother_name']
+    patient = request.POST['patient']
+    report = request.POST['report']
+    date = request.POST['date']
+    mother = request.POST['mother_name']
 
-    #dictionary_params = {}
-
-    #criar metodo para fazer busca no banco de dados
+    patients = search_patient(patient, report, date, mother)    
 
     return render_to_response(
         'search_results.html',
-        #{'patient': patient, 'report': report, 'date': date, 'mother': mother},
+        patients,
         context_instance=RequestContext(request)
     )
+
+def search_patient(patient_name, report_id, birth_date, mother_name):
+    patients_temp = Paciente.objects.using('hub').filter(nome__icontains = patient_name) & Paciente.objects.using('hub').filter(prontuario__icontains = report_id) & Paciente.objects.using('hub').filter(dt_nascimento__icontains = birth_date) & Paciente.objects.using('hub').filter(nome_mae__icontains = mother_name)
+
+    #Selecionando somente os 10 primeiros pacientes.    
+    patients = patients_temp[0:10]
+
+    return {"patients": patients}
+
 
 def home_search(request):
     return render_to_response(
