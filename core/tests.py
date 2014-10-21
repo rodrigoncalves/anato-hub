3a7c6a515d024810bde2950704b7175ed1ed0507
msfernandes <matheus.souza.fernandes@gmail.com>
2014-10-21 14:08:48 -0200

    Formatting test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
 
-# Create your tests here.
+
+class FormatTest():
+    def getName(self):
+        self.name = str(self.id).split('=')[-1][:-2]
+        self.name = self.name.split('test_')[-1]
+        self.name = self.name.replace('_', ' ')
+
+    def __str__(self):
+        self.getName()
+        out = '\r%s %s ' % (self.my_type, self.name.title())
+        out = out.ljust(71, '.')
+        return out + '\n'
