95c50f395704dc3e4fb43faedc45b002e47f29b9
cleitoncsg <cleitoncsg@gmail.com>
2014-10-22 00:59:37 -0200

    Add tests CytologyReport

--- cytology/tests.py
+++ cytology/tests.py
@@ -12,7 +12,9 @@ class CytologyTest(TestCase):
 			microscopic = "miscroscopia",
 			conclusion = "conclusao",
 			note = "nota",
-			footer = "legenda"
+			footer = "legenda",
+			status = "status",
+			exam = "exame"
 		)
 
 		cytology.save()
@@ -23,3 +25,31 @@ class CytologyTest(TestCase):
 		self.assertEquals("conclusao", cytology.conclusion)
 		self.assertEquals("nota", cytology.notes)
 		self.assertEquals("legenda", cytology.footer)
+		self.assertEquals("status", cytology.status)
+		self.assertEquals("exame", cytology.exam)
+
+class CytologyReportTest(TestCase):
+
+	def cytology_report_test(self):
+		cytology_report = CytologyReport(
+			clinical_information = "clinica",
+			quantity = "quantidade",
+			microscopic = "miscroscopia",
+			conclusion = "conclusao",
+			note = "nota",
+			footer = "legenda",
+			status = "status",
+			citology = "citologia"
+		)
+
+		cytologyReport.save()
+
+		self.assertEquals("clinica", cytology_report.clinical_information)
+		self.assertEquals("quantidade", cytology_report.quantity)
+		self.assertEquals("miscroscopia", cytology_report.microscopic)
+		self.assertEquals("conclusao", cytology_report.conclusion)
+		self.assertEquals("nota", cytology_report.notes)
+		self.assertEquals("legenda", cytology_report.footer)
+		self.assertEquals("status", cytology_report.status)
+		self.assertEquals("citologia", cytology_report.cytology)
+
