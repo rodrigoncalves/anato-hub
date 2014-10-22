e843e6811ee30ac7149ccec41c2503b2db734417
cleitoncsg <cleitoncsg@gmail.com>
2014-10-21 23:46:13 -0200

    Create test immunohistochemical

--- immunohistochemical/tests.py
+++ immunohistochemical/tests.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.db import models
+#from immunohistochemical import Immunohistochemical
 
-# Create your tests here.
+class ImmunohistochemicalTest(TestCase):
+
+	def immunohistochemical_test(self):
+		immunohistochemical = Immunohistochemical(
+			clinical_information= "clinica",
+			previous_biopsy = "previsao",
+			conclusion  = "conclusao",
+			note = "nota",
+			footer= "rodape"
+		)
+
+		immunohistochemical.save()
+
+		self.assertEquals("clinica",immunohistochemical.clinical_information)
+		self.assertEquals("previsao",immunohistochemical.previous_biopsy)
+		self.assertEquals("conclusao",immunohistochemical.conclusion)
+		self.assertEquals("nota",immunohistochemical.note)
+		self.assertEquals("rodape",immunohistochemical.footer)
+
+		#Verificar se é necessário incluir esses atributos
+		#status = models.ForeignKey(ImmunohistochemicalStatus)
+		#exam = models.ForeignKey(Exam)
