553e31541b3a0eeb8b91d09d804ce9c21fcada13
cleitoncsg <cleitoncsg@gmail.com>
2014-10-22 00:03:09 -0200

    immunohistochemical/tests.py

--- immunohistochemical/tests.py
+++ immunohistochemical/tests.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 from django.db import models
-#from immunohistochemical import Immunohistochemical
 
 class ImmunohistochemicalTest(TestCase):
 
@@ -11,7 +10,9 @@ class ImmunohistochemicalTest(TestCase):
 			previous_biopsy = "previsao",
 			conclusion  = "conclusao",
 			note = "nota",
-			footer= "rodape"
+			footer= "rodape",
+			status = "status",
+			exam = "exame"
 		)
 
 		immunohistochemical.save()
@@ -21,7 +22,5 @@ class ImmunohistochemicalTest(TestCase):
 		self.assertEquals("conclusao",immunohistochemical.conclusion)
 		self.assertEquals("nota",immunohistochemical.note)
 		self.assertEquals("rodape",immunohistochemical.footer)
-
-		#Verificar se é necessário incluir esses atributos
-		#status = models.ForeignKey(ImmunohistochemicalStatus)
-		#exam = models.ForeignKey(Exam)
+		self.assertEquals("status",immunohistochemical.status)
+		self.assertEquals("exame",immunohistochemical.exam)
