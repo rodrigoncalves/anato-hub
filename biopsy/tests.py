546c25b2383dd0c3542471e3ea98ff4d17c96479
cleitoncsg <cleitoncsg@gmail.com>
2014-10-23 12:59:42 -0200

    Refactoring Immunohistochemical test

--- biopsy/tests.py
+++ biopsy/tests.py
@@ -7,6 +7,7 @@ from models import BiopsyReport
 class BiopsyTest(TestCase):
 
 	def test_biopsy(self):
+		
 		biopsy = Biopsy(
 			clinical_information= 'clinica',
 			macroscopic= 'macroscopia',
--- immunohistochemical/tests.py
+++ immunohistochemical/tests.py
@@ -1,26 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 from django.db import models
+from models import Immunohistochemical
 
 class ImmunohistochemicalTest(TestCase):
 
-	def immunohistochemical_test(self):
+	def test_immunohistochemical(self):
+
 		immunohistochemical = Immunohistochemical(
 			clinical_information= "clinica",
 			previous_biopsy = "biopsia anterior",
 			conclusion  = "conclusao",
 			note = "nota",
-			footer= "legenda",
-			status = "status",
-			exam = "exame"
+			footer= "legenda"
 		)
-
-		immunohistochemical.save()
 
 		self.assertEquals("clinica",immunohistochemical.clinical_information)
 		self.assertEquals("biopsia anterior",immunohistochemical.previous_biopsy)
 		self.assertEquals("conclusao",immunohistochemical.conclusion)
 		self.assertEquals("nota",immunohistochemical.note)
 		self.assertEquals("legenda",immunohistochemical.footer)
-		self.assertEquals("status",immunohistochemical.status)
-		self.assertEquals("exame",immunohistochemical.exam)
