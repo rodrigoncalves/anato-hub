40d8c40c26dbb943d47eb83f9c3626d1e8810a98
cleitoncsg <cleitoncsg@gmail.com>
2014-10-23 12:34:29 -0200

    Refactoring biopsy test

--- biopsy/tests.py
+++ biopsy/tests.py
@@ -1,55 +1,42 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 from django.db import models
-from biopsy.models import Biopsy
+from models import Biopsy
+from models import BiopsyReport
 
 class BiopsyTest(TestCase):
 
-	def biopy_test(self):
+	def test_biopsy(self):
 		biopsy = Biopsy(
-			clinical_information= "clinica",
-			macroscopic= "macroscopia",
-			microscopic= "microscopia",
-			conclusion= "conclusao",
-			notes= "nota",
-			footer= "legenda",
-			status = "status",
-			exam = "exame"
+			clinical_information= 'clinica',
+			macroscopic= 'macroscopia',
+			microscopic= 'microscopia',
+			conclusion= 'conclusao',
+			note = 'nota',
+			footer= 'legenda'
 		)
 
-		biopsy.save()
-
-		self.assertEquals("clinica",biopsy.clinical_information)
-		self.assertEquals("macroscopia",biopsy.macroscopic)
-		self.assertEquals("microscopia",biopsy.microscopic)
-		self.assertEquals("conclusao",biopsy.conclusion)
-		self.assertEquals("nota",biopsy.notes)
-		self.assertEquals("legenda",biopsy.footer)
-		self.assertEquals("status",biopsy.status)
-		self.assertEquals("exame",biopsy.exam)
+		self.assertEquals('clinica',biopsy.clinical_information)
+		self.assertEquals('macroscopia',biopsy.macroscopic)
+		self.assertEquals('microscopia',biopsy.microscopic)
+		self.assertEquals('conclusao',biopsy.conclusion)
+		self.assertEquals('nota',biopsy.note)
+		self.assertEquals('legenda',biopsy.footer)
 
 
 class BiopsyReportTest(TestCase):
 
-	def biopy_report_test(self):
+	def test_biopy_report(self):
 		biopsy_report = BiopsyReport(
 			clinical_information= "clinica",
 			macroscopic= "macroscopia",
 			microscopic= "microscopia",
 			conclusion= "conclusao",
-			notes= "nota",
-			status = "status",
-			exam = "exame",
-			biopsy = "biopsia"
+			note= "nota",
 		)
-
-		biopsy_report.save()
 
 		self.assertEquals("clinica",biopsy_report.clinical_information)
 		self.assertEquals("macroscopia",biopsy_report.macroscopic)
 		self.assertEquals("microscopia",biopsy_report.microscopic)
 		self.assertEquals("conclusao",biopsy_report.conclusion)
-		self.assertEquals("nota",biopsy_report.notes)
-		self.assertEquals("status",biopsy_report.status)
-		self.assertEquals("exameeee",biopsy_report.exam)
-		self.assertEquals("biopsia",biopsy_report.biopsy)
+		self.assertEquals("nota",biopsy_report.note)
