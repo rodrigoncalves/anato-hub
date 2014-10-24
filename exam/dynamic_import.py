ecbe37bf844d587e071b5c2caed3dfde15a2daf7
msfernandes <matheus.souza.fernandes@gmail.com>
2014-10-24 09:55:09 -0200

    Dynamic import specific exam
    Signed-off-by: Ana Paula <anapaulavnoronha@gmail.com>
    Signed-off-by: Bryan <bryan.h.fernandes@hotmail.com>
    Signed-off-by: Matheus <matheus.souza.fernandes@gmail.com>

--- exam/dynamic_import.py
+++ exam/dynamic_import.py
@@ -15,3 +15,17 @@ def create_specific_exam(name_class):
         raise ModelDoesNotExist('Model does not exist')
     except ValueError:
         raise InvalidParameter('Invalid parameter')
+
+
+def import_class(exam_type):
+    try:
+        app_name = exam_type.name_class.lower()
+
+        module = __import__(
+            app_name + '.models', fromlist=[exam_type.name_class])
+        class_ = getattr(module, exam_type.name_class)
+        return class_
+    except ImportError:
+        raise ModelDoesNotExist('Model does not exist')
+    except ValueError:
+        raise InvalidParameter('Invalid parameter')
