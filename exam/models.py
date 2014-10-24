8a272e0c634336e3a472011dbfe23b9b7ac3119f
Ana Paula Vargas <anapaulavnoronha@gmail.com>
2014-10-24 12:47:08 -0200

    Defined new property_exam

--- exam/models.py
+++ exam/models.py
@@ -16,6 +16,11 @@ class Exam(models.Model):
         class_ = import_class(self.exam_type)
         return class_.objects.get(exam=self)
 
+    def get_patient_information(self):
+        from patient.models import Paciente
+        patient = Paciente.objects.using("hub").get(codigo=self.patient)
+        return patient
+
     request_date = models.DateField()
     receipt_date = models.DateField()
     speciment_collection_date = models.DateField()
@@ -26,6 +31,7 @@ class Exam(models.Model):
     exam_type = models.ForeignKey(ExamType)
     patient = models.BigIntegerField()
     specific_exam = property(get_specific_exam)
+    patient_information = property(get_patient_information)
 
 
 class ReportStatus(models.Model):
