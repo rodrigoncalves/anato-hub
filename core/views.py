dd6752acf980a428c0289b634f09d6d8e3a4fbb5
Rodrigo Gonçalves <rodrigosg2000@hotmail.com>
2014-10-29 16:29:40 -0200

    Delete unused function

--- core/views.py
+++ core/views.py
@@ -11,14 +11,8 @@ def home_search(request):
         context_instance=RequestContext(request)
     )
 
-def access_denied(request):
-    return render_to_response(
-        'access_denied.html',
-        context_instance=RequestContext(request)
-    )
-
 def user_belongs_to_groups(user, groups):
-	if user.groups.filter(name__in=groups).exists():
-		return True
-
-	return False
+    if user.groups.filter(name__in=groups).exists():
+        return True
+    else:
+        return False
