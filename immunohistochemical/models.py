c2e0107ac43f2be62c65d3c95e2d41909c25f71b
msfernandes <matheus.souza.fernandes@gmail.com>
2014-11-12 20:39:04 -0200

    Initializing immunohistochemical

--- immunohistochemical/models.py
+++ immunohistochemical/models.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from exam.models import Exam, ReportStatus
-
-
-class ImmunoHistochemicalStatus(models.Model):
-    description = models.CharField(max_length=50)
+from exam.models import Exam
 
 
 class ImmunoHistochemical(models.Model):
@@ -18,7 +14,6 @@ class ImmunoHistochemical(models.Model):
     conclusion = models.TextField(null=True, blank=True)
     note = models.TextField(null=True, blank=True)
     footer = models.TextField(null=True, blank=True)
-    status = models.ForeignKey(ImmunoHistochemicalStatus, default=1)
     exam = models.ForeignKey(Exam)
     antibodies = property(exam_antibodies)
 
@@ -27,13 +22,4 @@ class AntibodiesTable(models.Model):
     antibody = models.CharField(max_length=100)
     clone = models.CharField(max_length=100)
     result = models.CharField(max_length=255)
-    immunohistochemical = models.ForeignKey(ImmunoHistochemical)
-
-
-class ImmunoHistochemicalReport(models.Model):
-    clinical_information = models.TextField(null=True, blank=True)
-    previous_biopsy = models.CharField(max_length=50, null=True, blank=True)
-    conclusion = models.TextField(null=True, blank=True)
-    note = models.TextField(null=True, blank=True)
-    status = models.ForeignKey(ReportStatus)
     immunohistochemical = models.ForeignKey(ImmunoHistochemical)
