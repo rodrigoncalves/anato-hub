2da1378918c5696a23e7b74e571e0a0ed92d3427
Ana Paula Vargas <anapaulavnoronha@gmail.com>
2014-11-19 15:31:03 -0200

    Update bdd search patients

--- patients/features/steps/search.py
+++ patients/features/steps/search.py
@@ -16,138 +16,142 @@ def authentication(context):
     submit.click()
 
 @given(u'que o Usuario esta na tela de busca de pacientes')
-def is_on_home_search(context):
-    #Alterar porta do localhost
+def search_screen(context):
     context.driver.get('http://localhost:8000/consulta/')
     context.driver.title | should | equal_to('Home | Anato')
 
-@given(u'aparece a tela de busca')
-def step_impl(context):
-    assert False
-
 @when(u'o usuario digita o nome do Paciente')
-def typing_pacient_name(context):
+def patient_name(context):
     pacient_name_input = context.driver.find_element_by_id('patient')
-    pacient_name_input.send_keys('Queilane')
+    pacient_name_input.send_keys('MARIA EXXXXXXXXXXXXXXLEAL')
 
 @when(u'clica em buscar')
-def click_search_button(context):
+def search_click(context):
     search_button = context.driver.find_element_by_id('search-button')
     search_button.click()
 
 @then(u'o sistema retorna os Pacientes com o nome digitado')
-def return_pacient_name(context):
+def return_patient_name(context):
     pacient_name = context.driver.find_element_by_class_name('fi-male')
-    pacient_name.text | should | equal_to('QUEILANE BXXXISTA')
+    pacient_name.text | should | equal_to('MARIA EXXXXXXXXXXXXXXLEAL')
     context.driver.close()
 
 @when(u'o usuario digita o prontuario do Paciente')
-def typing_pacient_report(context):
+def patient_report(context):
     pacient_name_input = context.driver.find_element_by_id('report')
-    pacient_name_input.send_keys('808790')
+    pacient_name_input.send_keys('5754106')
 
 @then(u'o sistema retorna os Pacientes com o prontuario digitado')
-def return_pacient_report(context):
+def return_patient_report(context):
     pacient_report = context.driver.find_element_by_class_name('fi-male')
-    pacient_report.text | should | equal_to('LEONARDO QXXXXXXXXXXXXILVA')
+    pacient_report.text | should | equal_to('MARIA EXXXXXXXXXXXXXXLEAL')
     pacient_report = context.driver.find_element_by_class_name('fi-clipboard-notes')
-    pacient_report.text | should | equal_to('808790')
+    pacient_report.text | should | equal_to('5754106')
     context.driver.close()
 
 @when(u'o usuario digita o nome da mae do Paciente')
-def typing_patient_mothername(context):
+def patient_mother_name(context):
     pacient_mothername = context.driver.find_element_by_id('mother_name')
-    pacient_mothername.send_keys('Marfiza')
+    pacient_mothername.send_keys('MARILENE XXXXXXXXXXXOMES')
 
 @then(u'o sistema retorna os Pacientes com o nome da mae digitado')
-def return_patient_mothername(context):
-    patients_name = context.driver.find_elements_by_class_name('fi-male')
-    patients_mothername = context.driver.find_elements_by_class_name('fi-torso-female')
+def return_mother_name(context):
+    patients_name = context.driver.find_element_by_class_name('fi-male')
+    patients_mothername = context.driver.find_element_by_class_name('fi-torso-female')
 
-    patients_name[0].text | should | equal_to('ARIDEU CXXXXXXXXXXOPES')
-    patients_mothername[0].text | should | equal_to('MARFIZA XXXXXXXXXXOPES')
-    
-    patients_name[2].text | should | equal_to('NILZA MXXXXXXXXXXXNTOS') 
-    patients_mothername[1].text | should | equal_to('MARFIZA XXXXXXXAQUI')
-      
+    patients_name.text | should | equal_to('MARIA EXXXXXXXXXXXXXXLEAL')
+    patients_mothername.text | should | equal_to('MARILENE XXXXXXXXXXXOMES')
+        
     context.driver.close()
 
 @when(u'o usuario digita a data de nascimento do Paciente')
-def typing_birthday_only(context):
-    assert False
+def patient_born_date(context):
+    date_input = context.driver.find_element_by_id('date')
+    date_input.send_keys('16/06/2005')
 
 @then(u'o sistema retorna os Pacientes com a data de nascimento digitada')
-def step_impl(context):
-    assert False
-
-@when(u'o usuario digita o nome e prontuario do Paciente')
-def step_impl(context):
-    assert False
-
-@when(u'o usuario digita o nome, prontuario e nome da mae')
-def typing_patient_name_report_mothername(context):
-    name_input = context.driver.find_element_by_id('patient')
-    report_input = context.driver.find_element_by_id('report')
-    mothername_input = context.driver.find_element_by_id('mother_name')
-    
-    name_input.send_keys('Queilane')
-    report_input.send_keys('417899')
-    mothername_input.send_keys('Nao')
-
-@then(u'o sistema retorna os Pacientes com o nome, prontuario e nome da mae digitado')
-def return_name_report_mothername(context):
-    patient_name = context.driver.find_element_by_class_name('fi-male')
-    patient_report = context.driver.find_element_by_class_name('fi-clipboard-notes')
-    patient_mothername = context.driver.find_element_by_class_name('fi-torso-female')
-
-    patient_name.text | should | equal_to('QUEILANE BXXXISTA')
-    patient_report.text | should | equal_to('417899')
-    patient_mothername.text | should | equal_to('NAO XXXXXXMADO')
-
+def return_born_date(context):
+    date = context.driver.find_element_by_xpath("//span[3]/i")
+    date.text | should | start_with('16 de Junho de 2005')
     context.driver.close()
 
 @then(u'o sistema retorna os Pacientes com a nome e prontuario digitado')
-def step_impl(context):
-    assert False
-
-@when(u'o usuario digita o nome e nome da mae do Paciente')
-def step_impl(context):
-    assert False
+def return_name_report(context):
+    return_patient_name(context)
+    return_patient_report(context)
+    context.driver.close()
 
 @then(u'o sistema retorna os Pacientes com a nome e nome da mae digitado')
-def step_impl(context):
-    assert False
-
-@when(u'o usuario digita o nome e data de nascimento do Paciente')
-def step_impl(context):
-    assert False
+def return_name_mother_name(context):
+    return_patient_name(context)
+    return_mother_name(context)
+    context.driver.close()
 
 @then(u'o sistema retorna os Pacientes com a nome e data de nascimento digitado')
-def step_impl(context):
-    assert False
-
-@when(u'o usuario digita o prontuario e nome da mae do Paciente')
-def step_impl(context):
-    assert False
+def return_name_born_date(context):
+    return_patient_name(context)
+    return_born_date(context)
+    context.driver.close()
 
 @then(u'o sistema retorna os Pacientes com a prontuario e nome da mae digitado')
-def step_impl(context):
-    assert False
-
-@when(u'o usuario digita o prontuario e data nascimento do Paciente')
-def step_impl(context):
-    assert False
+def return_report_mother_name(context):
+    return_patient_report(context)
+    return_mother_name(context)
+    context.driver.close()
 
 @then(u'o sistema retorna os Pacientes com a prontuario e data nascimento digitado')
-def step_impl(context):
-    assert False
-#Implementando
-@when(u'o usuario digita o nome da mae e data nascimento do Paciente')
-def step_impl(context):
-    context.driver.find_element_by_id("date").click()
-    context.driver.find_element_by_xpath("//div[2]/div[3]/table/tbody/tr[3]/td[2]").click()
+def return_report_born_date(context):
+    return_patient_report(context)
+    return_born_date(context)
     context.driver.close()
 
 @then(u'o sistema retorna os Pacientes com a nome da mae e data nascimento digitado')
+def return_mother_name_born_date(context):
+    return_mother_name(context)
+    return_born_date(context)
+    context.driver.close()
+
+@then(u'o sistema retorna os Pacientes com o nome, prontuario e nome da mae digitado')
+def name_report_mother_name(context):
+    return_patient_name(context)
+    return_patient_report(context)
+    return_mother_name(context)
+    context.driver.close()
+
+@then(u'o sistema retorna os Pacientes com o nome, prontuario e data de nascimento digitado')
+def return_name_report_born_date(context):
+    return_patient_name(context)
+    return_patient_report(context)
+    return_born_date(context)
+    context.driver.close()
+
+@then(u'o sistema retorna os Pacientes com o nome, nome da mãe e data de nascimento digitado')
+def return_name_mother_name_born_date(context):
+    return_patient_name(context)
+    return_mother_name(context)
+    return_born_date(context)
+    context.driver.close()
+
+@then(u'o sistema retorna os Pacientes com o prontuario, nome da mae e data de nascimento digitado')
+def return_report_mother_name_born_date(context):
+    return_patient_report(context)
+    return_mother_name(context)
+    return_born_date(context)
+    context.driver.close()
+
+@then(u'o sistema retorna os Pacientes com o nome, prontuario, nome da mae e data de nascimento digitado')
+def return_all_fields(context):
+
+    return_patient_name(context)
+    return_patient_report(context)
+    return_mother_name(context)
+    return_born_date(context)
+    context.driver.close()
+
+@when(u'o usuario nao digita nenhum dos campos')
 def step_impl(context):
-    assert False
+    pass
+
+@then(u'o botao de busca deve estar desabilitado')
+def step_impl(context):
+    pass
