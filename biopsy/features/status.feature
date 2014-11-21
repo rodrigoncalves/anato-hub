b0572b304eca9e99e24119e92653c9ce6ebe1a2e
Rodrigo Gonçalves <rodrigosg2000@hotmail.com>
2014-11-21 00:30:32 -0200

    Fixing exam and biopsy scenarios

--- biopsy/features/status.feature
+++ biopsy/features/status.feature
@@ -1,4 +1,11 @@
- E aparece a tela com a listagem de exames do paciente
+Funcionalidade: Eu, como auxiliar,
+Desejo ver o status do exame
+Para saber em que parte da analise ele se encontra.
+
+    Cenário: Exame biopsia no status Em Macroscopia
+    Dado que o auxiliar acessa o sistema na pagina do paciente e esta autenticado
+    E aparece a tela com a listagem de exames do paciente
+    E aparece a tela com a listagem de exames do paciente
     Quando o  auxiliar clica no exame de biopsia
     Então abre os dados do exame
     E mostra o status como Em Macroscopia
--- exam/features/steps/update_exam.py
+++ exam/features/steps/update_exam.py
@@ -38,8 +38,6 @@ def click_edit_button(context):
 def update_fields(context):
 	context.driver.find_element_by_id("received_speciment").clear()
 	context.driver.find_element_by_id("received_speciment").send_keys("Material")
-	context.driver.find_element_by_id("examination_time").clear()
-	context.driver.find_element_by_id("examination_time").send_keys("0000")
 	context.driver.find_element_by_id("requesting_physician").clear()
 	context.driver.find_element_by_id("requesting_physician").send_keys("Medico")
 	context.driver.find_element_by_id("responsible_physician").clear()
@@ -57,8 +55,6 @@ def step_impl(context):
 @when(u'atualiza todos os campos e deixa o material recebido em branco')
 def update_all_less_material(context):
 	context.driver.find_element_by_id("received_speciment").clear()
-	context.driver.find_element_by_id("examination_time").clear()
-	context.driver.find_element_by_id("examination_time").send_keys("0000")
 	context.driver.find_element_by_id("requesting_physician").clear()
 	context.driver.find_element_by_id("requesting_physician").send_keys("Medico")
 	context.driver.find_element_by_id("responsible_physician").clear()
@@ -73,7 +69,6 @@ def step_impl(context):
 def update_all_less_examination_time(context):
 	context.driver.find_element_by_id("received_speciment").clear()
 	context.driver.find_element_by_id("received_speciment").send_keys("Material")
-	context.driver.find_element_by_id("examination_time").clear()
 	context.driver.find_element_by_id("requesting_physician").clear()
 	context.driver.find_element_by_id("requesting_physician").send_keys("Medico")
 	context.driver.find_element_by_id("responsible_physician").clear()
@@ -83,8 +78,6 @@ def update_all_less_examination_time(context):
 def update_all_less_requesting_physician(context):
 	context.driver.find_element_by_id("received_speciment").clear()
 	context.driver.find_element_by_id("received_speciment").send_keys("Material")
-	context.driver.find_element_by_id("examination_time").clear()
-	context.driver.find_element_by_id("examination_time").send_keys("0000")
 	context.driver.find_element_by_id("requesting_physician").clear()
 	context.driver.find_element_by_id("responsible_physician").clear()
 	context.driver.find_element_by_id("responsible_physician").send_keys("Responsavel")
@@ -93,8 +86,6 @@ def update_all_less_requesting_physician(context):
 def update_all_less_requesting_physician(context):
 	context.driver.find_element_by_id("received_speciment").clear()
 	context.driver.find_element_by_id("received_speciment").send_keys("Material")
-	context.driver.find_element_by_id("examination_time").clear()
-	context.driver.find_element_by_id("examination_time").send_keys("0000")
 	context.driver.find_element_by_id("requesting_physician").clear()
 	context.driver.find_element_by_id("requesting_physician").send_keys("Medico")
 	context.driver.find_element_by_id("responsible_physician").clear()
