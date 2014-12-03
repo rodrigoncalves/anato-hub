a8bfc655b14a20c4cf587c4188ca0b932c00a543
BryanFernandes <bryan.h.fernandes@hotmail.com>
2014-12-03 03:43:45 -0200

    New interface implement. Partial refactoring code. Bug fixes

--- immunohistochemical/views.py
+++ immunohistochemical/views.py
@@ -1,27 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template.context import RequestContext
 from django.contrib.auth.decorators import login_required
+from core.decorators import permission_required_with_403
 from models import ImmunoHistochemical
 
 
 @login_required(login_url='/', redirect_field_name='')
 def register_immunohistochemical(request):
-    clinical_information = request.POST.get('clinical_information')
-    previous_biopsy = request.POST.get('previous_biopsy')
-    conclusion = request.POST.get('conclusion')
-    note = request.POST.get('note')
-    footer = request.POST.get('footer')
-
-    immunohistochemical = ImmunoHistochemical(
-        clinical_information=clinical_information,
-        previous_biopsy=previous_biopsy,
-        conclusion=conclusion,
-        note=note,
-        footer=footer
-    )
+    immunohistochemical = ImmunoHistochemical.objects.get(pk=request.POST['immunohistochemical_id'])
+    immunohistochemical.clinical_information = request.POST['clinical_information']
+    immunohistochemical.previous_biopsy = request.POST['previous_biopsy']
+    immunohistochemical.conclusion = request.POST['conclusion']
+    immunohistochemical.note = request.POST['note']
+    immunohistochemical.footer = request.POST['footer']
 
     immunohistochemical.save()
 
-    return redirect('/exame/%d' % immunohistochemical.exam.id)
+    return redirect('/exame/' + str(immunohistochemical.exam.id))
